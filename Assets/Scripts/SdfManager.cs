52a02dbd1abcec8fcf26157b21917b1159f7a9ab
Bronson Zgeb <bronson.zgeb@gmail.com>
2022-01-03 14:47:29 -0500

    Initial commit

---
+++ Assets/Scripts/SdfManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SdfManager : MonoBehaviour
+{
+    Renderer _renderer;
+    MaterialPropertyBlock _materialPropertyBlock;
+
+    const int MaxShapes = 256;
+    int _numShapes;
+}
